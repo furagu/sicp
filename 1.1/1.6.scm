416d0344f3ce657f86edb8f782be880057f466a6
Egor Balyshev <frag.trials@gmail.com>
2014-01-28 22:17:40 +0800

    Exercise 1.6

---
+++ 1.1/1.6.scm
@@ -0,0 +1,31 @@
+(load "./chapter.scm")
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+(display (sqrt 2)) ; This will leed to infinite recursion of sqrt-iter procedure caused by applicative order of evaluation.
---
+++ 1.1/chapter.scm
@@ -0,0 +1,6 @@
+(define (square x) (* x x))
+
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
